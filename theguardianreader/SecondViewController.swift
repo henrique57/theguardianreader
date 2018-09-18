06887ddd0ac64192c54e519bd3184198de4efd51
henriqueStefanini <hp.metal57@gmail.com>
2018-09-18 10:54:15 -0300

    Initial Commit

---
+++ theguardianreader/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  theguardianreader
+//
+//  Created by Henrique Pereira on 18/09/2018.
+//  Copyright © 2018 Henrique Pereira. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
